5921f535823c2ea45b830fc2fcbe42345210b5f7
Gamze Islamoglu <gamzeisl@hotmail.com>
2021-07-01 23:03:15 +0200

    [software] Add barrier and single

--- software/runtime/synchronization.c
+++ software/runtime/synchronization.c
@@ -13,6 +13,7 @@
 uint32_t volatile barrier __attribute__((section(".l1")));
 uint32_t volatile barrier_iteration __attribute__((section(".l1")));
 uint32_t volatile barrier_init __attribute__((section(".l2"))) = 0;
+uint32_t volatile barrier_init_gomp __attribute__((section(".l2"))) = 0;
 
 void mempool_barrier_init(uint32_t core_id, uint32_t num_cores) {
   if (core_id == 0) {
@@ -50,5 +51,31 @@ void mempool_barrier(uint32_t num_cores, uint32_t cycles) {
     // Some threads have not reached the barrier --> Let's wait
     while (iteration_old == barrier_iteration)
       mempool_wait(cycles);
+  }
+}
+
+void mempool_barrier_gomp(uint32_t core_id, uint32_t num_cores) {
+  if (barrier_init_gomp == 0){
+    if (core_id == 0) {
+      barrier = 0;
+      barrier_iteration = 0;
+      barrier_init = 1;
+      barrier_init_gomp = 1;
+    } else {
+      while (!barrier_init)
+	mempool_wait(2 * num_cores);
+    }
+  }
+  // Remember previous iteration
+  uint32_t iteration_old = barrier_iteration;
+
+  // Increment the barrier counter
+  if ((num_cores - 1) == __atomic_fetch_add(&barrier, 1, __ATOMIC_SEQ_CST)) {
+    barrier = 0;
+    __atomic_fetch_add(&barrier_iteration, 1, __ATOMIC_SEQ_CST);
+  } else {
+    // Some threads have not reached the barrier --> Let's wait
+    while (iteration_old == barrier_iteration)
+      mempool_wait(num_cores*2);
   }
 }
